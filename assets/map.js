e77406d147df4ae10dd87e9e13c34d6e1a7f4d00
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-08-16 10:41:42 +0200

    Add geocoder

--- assets/map.js
+++ assets/map.js
@@ -1,9 +1,52 @@
 let protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
+const geocoderApi = {
+  forwardGeocode: async (config) => {
+    const features = [];
+    try {
+      const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
+      const response = await fetch(request);
+      const geojson = await response.json();
+      for (const feature of geojson.features) {
+        const center = [
+          feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+          feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
+        ];
+        const point = {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: center,
+          },
+          place_name: feature.properties.display_name,
+          properties: feature.properties,
+          text: feature.properties.display_name,
+          place_type: ["place"],
+          center,
+        };
+        features.push(point);
+      }
+    } catch (e) {
+      console.error(`Failed to forwardGeocode with error: ${e}`);
+    }
+
+    return {
+      features,
+    };
+  },
+};
+
 const map = new maplibregl.Map({
   container: "map",
 });
+map.addControl(
+  new MaplibreGeocoder(geocoderApi, {
+    maplibregl,
+  }),
+  "top-left",
+);
+
 map.addControl(new maplibregl.NavigationControl());
 
 map.on("style.load", () => {
