7c09f14e78bc61c9d4ec96074f2a8eb50bc2409b
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-08-17 11:04:25 +0200

    Improve geocoding

--- assets/map.js
+++ assets/map.js
@@ -5,27 +5,18 @@ const geocoderApi = {
   forwardGeocode: async (config) => {
     const features = [];
     try {
-      const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
+      const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson`;
       const response = await fetch(request);
       const geojson = await response.json();
       for (const feature of geojson.features) {
-        const center = [
-          feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
-          feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
-        ];
-        const point = {
+        features.push({
           type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: center,
-          },
+          geometry: feature.geometry,
           place_name: feature.properties.display_name,
           properties: feature.properties,
           text: feature.properties.display_name,
           place_type: ["place"],
-          center,
-        };
-        features.push(point);
+        });
       }
     } catch (e) {
       console.error(`Failed to forwardGeocode with error: ${e}`);
