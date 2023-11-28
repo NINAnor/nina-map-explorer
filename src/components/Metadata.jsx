3c3e6503de6ec66eed8b66281d32d32b0c8462a9
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-11-28 10:52:43 +0100

    Use HTML expandable collapse

--- src/components/Metadata.jsx
+++ src/components/Metadata.jsx
@@ -1,10 +1,9 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import useMetadata from "../hooks/useMetadata";
 import MapContext from "../contexts/map";
 
 
 export default function Metadata() {
-  const [open, setOpen] = useState(false);
   const { style } = useContext(MapContext);
   const { data, isLoading } = useMetadata();
 
@@ -16,8 +15,10 @@ export default function Metadata() {
     <div style={{ marginLeft: '0.5rem' }}>
       <h3>{style ? style.name : ''}</h3>
       <p>{data.subtitle}</p>
-      <div onClick={() => setOpen(!open)}><i className={`fas fa-caret-${open ? 'down' : 'right'}`}></i> Beskrivelse</div>
-      {open ? <div style={{ marginLeft: '0.5rem'}} dangerouslySetInnerHTML={{ __html: data.description }} /> : null}
+      <details>
+        <summary>Beskrivelse</summary>
+        <div style={{ marginLeft: '0.5rem'}} dangerouslySetInnerHTML={{ __html: data.description }} />
+      </details>
     </div>
   )
 }
