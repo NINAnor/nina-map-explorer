6ac1dce938d29c36a4d40223ca42aa6174dfd0f3
Niccolò Cantù <niccolo.cantu@nina.no>
2024-02-26 10:43:49 +0100

    setup sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './components/plausible.js';
+import './components/sentry.js';
 import App from './App.jsx'
 import './styles/app.scss'
 
