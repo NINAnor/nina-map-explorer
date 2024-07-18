e2920c6854c3f86f755a6630d2759b0d4a13a299
Niccolò Cantù <niccolo.cantu@nina.no>
2024-07-18 11:13:02 +0200

    fix client height

--- src/pages/viewer/useClientHeight.js
+++ src/pages/viewer/useClientHeight.js
@@ -4,19 +4,16 @@ import useResizeObserver from "use-resize-observer";
 export default function useClientHeight() {
   const [height, setHeight] = useState(0);
   const ref = useRef(null);
-  const { height: bbh } = useResizeObserver(ref);
+  const { height: bbh } = useResizeObserver({ ref });
 
   useLayoutEffect(() => {
-    const height = ref.current.offsetHeight;
     // style extracts the declared margin, but it's CSS value so it has "px" suffix
     // rather then being an integer, parseInt casts it
     const style = window.getComputedStyle
       ? getComputedStyle(ref.current, null)
       : ref.current.currentStyle;
-    setHeight(
-      height + parseInt(style.marginBottom) + parseInt(style.marginTop),
-    );
-  }, [ref.current, bbh]);
+    setHeight(bbh + parseInt(style.marginBottom) + parseInt(style.marginTop));
+  }, [bbh]);
 
   return { height, ref };
 }
