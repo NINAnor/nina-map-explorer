25d6446f3f18af744dfd4cd26647125795858671
Niccolò Cantù <niccolo.cantu@nina.no>
2024-10-24 14:39:38 +0200

    add sentry sourcemaps setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,30 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const plugins = [];
+
+if (process.env.SENTRY_ORG) {
+  plugins.push(
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+    }),
+  );
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ...plugins],
+
   server: {
     host: true,
     port: 3000,
   },
+
   base: "./",
+
+  build: {
+    sourcemap: true,
+  },
 });
